bce19ff075efaf7d67c1b5a8e17efe1a2e88bb55
mjawn <25188486+mjawn@users.noreply.github.com>
2023-03-30 14:23:27 -0400

    WIP updating elventy config, adding tailwind

---
+++ .eleventy.config.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    return {
+      dir: {
+        input: "_input",
+        output: "_output"
+      }
+    }
+  };
---
+++ .tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./input/**/*.{html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
