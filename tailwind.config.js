ecbef6e9882f2aa49ec8e5133d57872a2504aa8e
mjawn <25188486+mjawn@users.noreply.github.com>
2023-03-31 22:33:59 -0400

    Starting on header markup and theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
 content: ["./src/**/*.{html,njk}"],
   theme: {
     extend: {},
+    colors: {
+        ivory: "#FDFBE8",
+        madder: "#A41623",
+        gunmetal: "#162931"
+    }
   },
   plugins: [],
 }
