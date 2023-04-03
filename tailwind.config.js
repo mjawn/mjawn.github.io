9e2f77c2598b19e271c607f5d714267b12541895
mjawn <25188486+mjawn@users.noreply.github.com>
2023-04-03 17:14:40 -0400

    Adding logo, testing various logotypes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-content: ["./src/**/*.{html,njk}"],
-  theme: {
-    extend: {},
-    colors: {
-        ivory: "#FDFBE8",
-        madder: "#A41623",
-        gunmetal: "#162931"
-    }
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,njk}"],
+    theme: {
+        colors: {
+            current: "currentColor",
+            inherit: 'inherit',
+            transparent: 'transparent',
+            ivory: "#FDFBE8",
+            madder: "#A41623",
+            gunmetal: "#162931"
+        },
+        fontFamily: {
+            sans: [...defaultTheme.fontFamily.sans],
+            serif: [...defaultTheme.fontFamily.serif],
+            mono: [...defaultTheme.fontFamily.mono],
+            AbrilFatface: ["Abril Fatface"],
+            YesevaOne: ["Yeseva One"],
+            Chonburi: ["Chonburi"],
+            PollerOne: ["Poller One"],
+            Limelight: ["Limelight"],
+            OrelegaOne: ["Orelega One"]
+        },
+        extend: {}
+    },
+    plugins: []
 }
