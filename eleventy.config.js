34168e49772bed78406131f41a778fd29ecd3090
mjawn <25188486+mjawn@users.noreply.github.com>
2023-04-10 09:31:23 -0400

    Correcting some webc props, updating title. Adding css minification script. Removing default comments.

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,12 +3,8 @@ const EleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
 module.exports = function (eleventyConfig) {
     // Plugins
     eleventyConfig.addPlugin(EleventyWebcPlugin, {
-        // Glob to find no-import global components
-        // This path is relative to the project-root!
-        // The default value is shown:
         components: "src/components/**/*.webc"
     });
-    // Return your Object options:
     return {
         dir: {
             input: "src",
