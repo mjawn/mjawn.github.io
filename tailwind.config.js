205331cb8903aa06076a5963cc8bd49c96502b89
mjawn <25188486+mjawn@users.noreply.github.com>
2024-09-11 17:21:09 -0400

    Style and content updates. Going back to using the default font size classes that add line-height. Adding all the repeated styles in index.webc to the stylesheet with @apply. Making better use of the cascade by adding stuff to body. Some text content updates and clarifications.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,18 +11,6 @@ module.exports = {
             gunmetal: "#162931",
             moonstone: "#0FA3B1"
         },
-        fontSize: {
-            xs: "0.75rem",
-            sm: "0.875rem",
-            base: "1rem",
-            lg: "1.125rem",
-            xl: "1.25rem",
-            "2xl": "1.5rem",
-            "3xl": "1.875rem",
-            "4xl": "2.25rem",
-            "5xl": "3rem",
-            "6xl": "4rem"
-        },
         extend: {
             borderWidth: {
                 5: "5px"
